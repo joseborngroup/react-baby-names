66b0ad122ab318843f81bcb0a539ca01412fc2db
Jose Paitamala <jose@formcommerce.com>
2021-07-13 15:58:33 +0100

    iterate through array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App({names}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <pre>{JSON.stringify(names, null, 2)}</pre>
+    <ul>
+      {
+        names.map((entry) => (
+           <li key={entry.id} className={entry.sex}>
+             <button>{entry.name}</button>
+           </li>
+        ))
+      }
+    </ul>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// CSS
+import 'normalize-css';
+import './css/styles.css';
+
+// Components
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Data
+import {names} from './data/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App names={names} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
